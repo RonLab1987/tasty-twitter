01764e15529ee64f47219601eed8d99d08d9d037
Roman Laptev <rlaptev@infoss.no>
2020-12-15 13:42:28 +0300

    Add Vuetify ui lib and configure build

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "../router";
+import { makeVuetify } from "../vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify: makeVuetify(false),
   router,
   render: h => h(App)
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+const { VuetifyLoaderPlugin } = require("vuetify-loader");
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  pages: {
+    main: {
+      entry: "src/app/index.ts",
+      template: "public/index.html",
+      title: "Tasty Twitter",
+      filename: "index.html"
+    }
+  },
+  configureWebpack: {
+    plugins: [new VuetifyLoaderPlugin()]
+  }
 };
